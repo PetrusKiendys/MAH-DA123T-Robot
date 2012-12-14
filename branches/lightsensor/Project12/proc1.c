0fea1bfbc6357d1f1c392e7c1795b3d9853a711c
petruskiendys@gmail.com <petruskiendys@gmail.com@8720de40-df1b-d2df-ad68-33ea94c1a2b3>
2012-12-14 11:49:29 +0000

    branching trunk to LIGHTSENSOR

---
+++ branches/lightsensor/Project12/proc1.c
@@ -0,0 +1,62 @@
+/*
+ * proc1.c
+ *
+ *  Created on: 31 okt 2011
+ *      Author: Tommy
+ */
+
+/*****************************************************************************
+ *
+ *    Exempel 1
+ *
+ *
+ ****************************************************************************/
+#include "pre_emptive_os/api/osapi.h"
+#include "general.h"
+#include "startup/lpc2xxx.h"
+#include "startup/printf_P.h"
+#include "startup/ea_init.h"
+#include "startup/consol.h"
+#include "startup/config.h"
+#include "startup/framework.h"
+
+#include "LCD/LCD.h"  //  Funktionsprototyper för LCD-rutinerna
+
+extern long const delayshort;
+extern long const delaylong;
+
+extern tCntSem mutexLCD;
+
+
+
+void
+procEx1(void* arg)
+{
+  tU8 error;
+
+  for(;;)
+  {
+// Exempelkod för att visa hur flera processer kan dela på LCD
+
+   osSemTake(&mutexLCD, 0, &error);
+
+	delay(delayshort);
+	send_instruction(1);	//cleara displayen
+	delay(delaylong);
+	send_instruction(2);  //cursorn till första positionen
+	delay(delaylong);
+    send_character('E');
+    delay(delayshort);
+    send_character('x');
+    delay(delayshort);
+
+    send_character('1');
+
+    osSleep(300);		// för att infon säkert skall hinna synas
+
+    osSemGive(&mutexLCD, &error);
+
+
+    osSleep(1000);
+  }
+}
