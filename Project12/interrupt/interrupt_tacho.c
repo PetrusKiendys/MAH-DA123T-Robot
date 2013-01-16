4f6cc0ff32d0d5fe037408377b5abaaf4e2ee4bc
mullenr7@gmail.com <mullenr7@gmail.com@8720de40-df1b-d2df-ad68-33ea94c1a2b3>
2013-01-16 14:53:36 +0000

    latest changes to "integrate_processes"

---
+++ Project12/interrupt/interrupt_tacho.c
@@ -0,0 +1,53 @@
+/*************************************************************
+ *  Filename: interrupt_tacho.c
+ *  Created on: 2013-01-14
+ *  Author: Petrus K
+ *************************************************************/
+
+/******************************************************************************
+ * Includes, defines, variable declarations and prototypes
+ *****************************************************************************/
+#include "../general.h"
+#include "../startup/lpc2xxx.h"
+#include "../startup/framework.h"
+#include "../startup/config.h"
+
+void Tacho1_ISR(void);
+void Tacho2_ISR(void);
+
+static tU32 tacho1_counter;
+static tU32 tacho2_counter;
+
+
+/******************************************************************************
+ * ISR
+ *****************************************************************************/
+void Tacho1_ISR(void) {
+	// perform this when Tacho1 is triggered
+	printf("\n\nTacho1 ISR: %d", tacho1_counter);
+	tacho1_counter++;
+
+	// don't forget to reset the interrupt flag!
+	EXTINT = 0x00000009;			// (re)activates EINT0 and EINT3
+
+	VICVectAddr = 0x00;				//dummy write to VIC to signal end of interrupt
+									// QUESTION: have to use this statement?
+									// EDIT: apparently so, see p. 58 4.12 Vector Address register of manualLPC2148.pdf for more info
+									//		 "Writing to this register does not set the value for future reads from it."
+									// 		 "Rather, this register should be written near the end of an ISR, to update the priority hardware."
+}
+
+void Tacho2_ISR(void) {
+	// perform this when Tacho2 is triggered
+	printf("\nTacho2 ISR: %d", tacho2_counter);
+	tacho2_counter++;
+
+	// don't forget to reset the interrupt flag!
+	EXTINT = 0x00000009;			// (re)activates EINT0 and EINT3
+
+	VICVectAddr = 0x00;				//dummy write to VIC to signal end of interrupt
+									// QUESTION: have to use this statement?
+									// EDIT: apparently so, see p. 58 4.12 Vector Address register of manualLPC2148.pdf for more info
+									//		 "Writing to this register does not set the value for future reads from it."
+									// 		 "Rather, this register should be written near the end of an ISR, to update the priority hardware."
+}
--- Project12/proc2.c
+++ Project12/proc2.c
@@ -19,50 +19,155 @@
 #include "startup/config.h"
 #include "startup/framework.h"
 
+
+//#include "i2c.h"
 #include "LCD/LCD.h"  //  Funktionsprototyper för LCD-rutinerna
+#include "I2C/i2c.h"  //  Funktionsprototyper för I2C-rutinerna
 
 extern long const delayshort;
 extern long const delaylong;
 
 extern tCntSem mutexLCD;
 
+/******************************************************************************
+ * Defines and typedefs
+ *****************************************************************************/
+#define CRYSTAL_FREQUENCY FOSC
+#define PLL_FACTOR        PLL_MUL
+#define VPBDIV_FACTOR     PBSD
+#define pin4			  0x00000010 //0b10000
+
+/*****************************************************************************
+ * Public function prototypes
+ ****************************************************************************/
+//int main(void);
+
+/*****************************************************************************
+ * Local function prototypes
+ ****************************************************************************/
+//static void delayMs(tU16 delayInMs);
+
+/*****************************************************************************
+ * Implementation of local functions
+ ****************************************************************************/
+
+/*****************************************************************************
+ *
+ * Description:
+ *    Delay execution by a specified number of milliseconds by using
+ *    timer #1. A polled implementation.
+ *
+ * Params:
+ *    [in] delayInMs - the number of milliseconds to delay.
+ *
+ ****************************************************************************/
+//static void delayMs(tU16 delayInMs) {
+	/*
+	 * setup timer #1 for delay
+	 */
+/*	TIMER1_TCR = 0x02;          //stop and reset timer
+	TIMER1_PR = 0x00;          //set prescaler to zero
+	TIMER1_MR0 = delayInMs
+			* ((CRYSTAL_FREQUENCY * PLL_FACTOR) / (1000 * VPBDIV_FACTOR));
+	TIMER1_IR = 0xff;          //reset all interrrupt flags
+	TIMER1_MCR = 0x04;          //stop timer on match
+	TIMER1_TCR = 0x01;          //start timer
+
+	//wait until delay time has elapsed
+	while (TIMER1_TCR & 0x01)
+		;
+}
+*/
+/*****************************************************************************
+ * If we want a delay of x µs we have to send 15•x with the function delay.
+ ****************************************************************************/
+//void delay(long pulses)
+//{
+//	asm("wait:   SUBS R0, R0, #1");
+//	asm("BNE wait");
+//}
+
+
+tU8 getDistance(void)
+{
+	tU8 data[1];
+
+	IODIR &= ~pin4; // make pin4 to input
+	IOCLR |= pin4; // make pin4 high
+
+	data[0] = 0; // 8 mest signifikanta bitarna
+
+	// Sending data to I2C network
+	i2cStart();
+	i2cWrite(0x02, 0x1, 0x42, data, 0); // 0x42 Command "Read Distance" send buffer
+	i2cStop();
+
+	delay(1500); // If we want a delay of x µs we have to send 15•x with the function delay.
+	IODIR |= pin4; 	// make pin4 to output
+	delay(750);
+	IODIR &= ~pin4; // make pin4 to input
+	delay(1500);
+
+	// Reading data from I2C network
+	i2cStart();
+	i2cRead(0x03, data, 1); // storing data in pin4
+	i2cStop();
+
+	//wait 1000 ms
+	//delayMs(1000);
+
+  return data[1];
+}
 
 void
 procEx2(void* arg)
 {
   tU8 error;
+  static int pauseFlag = 0;
+
 
   for(;;)
   {
 // Exempelkod för att visa hur flera processer kan dela på LCD
     osSemTake(&mutexLCD, 0, &error);
 
-    delay(delayshort);
-	send_instruction(1);	//cleara displayn
-	delay(delaylong);
-	send_instruction(2);  //cursorn till första positionen
-	delay(delaylong);
-    send_character('E');
-    delay(delayshort);
-    send_character('x');
-    delay(delayshort);
-    send_character('e');
-    delay(delayshort);
-    send_character('m');
-    delay(delayshort);
-    send_character('p');
-    delay(delayshort);
-    send_character('e');
-    delay(delayshort);
-    send_character('l');
-    delay(delayshort);
-    send_character('2');
+    //initialize printf()-functionality
+    	//eaInit();
 
-    osSleep(300);		// för att infon säkert skall hinna synas
+    	//print welcome message
+ //   	printf("Distance Measuring\n");
+
+    	//initialice i2c communication
+ //   	i2cInit(10000);
+
+    	//enter forever loop...
+   // 	while (1) {
+    		tU8 data[1];
+
+    		data[0] = getDistance();
+
+  //  		printf("\nDistance: %d units\n", data[0]);
+
+    		if( data[0] < 15 && pauseFlag == 0) {
+    			pauseState();
+    			pauseFlag = 1;
+    		} else if( data[0] >= 15 && pauseFlag == 1 ){
+    			stopPauseState();
+    			pauseFlag = 0;
+    		}
+
+    		//osSleep(300);
+    		osSleep(40);
+    	//}
 
     osSemGive(&mutexLCD, &error);
 
-    osSleep(2000);
+    osSleep(50);
+   // osSleep(1000);
+   // osSleep(300);
   }
 }
+
+//  }
+//}
 
